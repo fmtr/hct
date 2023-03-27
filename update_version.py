b30d4b14286a80999b4fc82a7accdfc0f5eefd26
Edward Brown <ed@frontmatter.ai>
2023-03-27 15:06:55 +0100

    Add ttads config, refine version regex

--- update_version.py
+++ update_version.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 PATH_ROOT = Path(__file__).parent.absolute()
 ENCODING = 'UTF-8'
-PATTERN_VERSION = r'\d+\.\d+\.\d+'
+PATTERN_VERSION = r'(?P<major>0|[1-9]\d*)\.(?P<minor>0|[1-9]\d*)\.(?P<patch>0|[1-9]\d*)(?:-(?P<pre>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?(?:\+(?P<build>[0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?'
 STEMS_MASKS = {
     'module/hct.be': r"VERSION\s*=\s*'({})'",
     # 'README.md': r"fmtr/hct/v({})",
@@ -23,7 +23,6 @@ if __name__ == '__main__':
         path = PATH_ROOT / stem
         text_old = path.read_text(encoding=ENCODING)
         pattern = mask.format(PATTERN_VERSION)
-        rx = re.compile(pattern)
         replacement = pattern.format(VERSION)
         text = re.sub(pattern, replacer, text_old)
         path.write_text(text, encoding=ENCODING)
